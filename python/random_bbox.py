c03d6cf83e9cd80f6c5df4dcd51b7ea35f273119
Akshay Joshi <akjoshi6@gmail.com>
2018-08-26 23:28:41 +0530

    Functiona name change

--- python/random_bbox.py
+++ python/random_bbox.py
@@ -3,11 +3,9 @@ import os
 import cv2
 import pandas as pd
 
-h, w = 345, 500
-
 def gen_random_boxes(h=345, w=500, n_boxes=5):
     boxes = []
-    for i in range(n_boxes):
+    for _ in range(n_boxes):
         x_max = random.randint(75, w)
         y_max = random.randint(75, h)
         x_min = random.randint(10, x_max-10)
@@ -15,7 +13,7 @@ def gen_random_boxes(h=345, w=500, n_boxes=5):
         boxes.append([x_min, y_min, x_max, y_max])
     return boxes
 
-def read_images(directory):
+def get_dataframe_with_bboxes(directory):
     all_data=pd.DataFrame()
     for filename in os.listdir(directory):
         if filename.endswith(".jpg") or filename.endswith(".jpeg") or filename.endswith(".JPG") or filename.endswith(".png"):
@@ -33,6 +31,6 @@ def read_images(directory):
     return all_data
 
 if __name__ == '__main__':
-    aug_data=read_images(DIR_PATH)
+    aug_data=get_dataframe_with_bboxes(DIR_PATH)
     aug_data.to_csv(FILFENAME+".csv",encoding="utf-8",index=False)
     
